4d6b1cb7bf968526a2c002225f5ca258693327c9
MayssenBHA <154880238+MayssenBHA@users.noreply.github.com>
2025-03-30 01:35:47 +0100

    Delete part-time-connect-backend_django/PartTimeConnect_backend directory

--- part-time-connect-backend_django/PartTimeConnect_backend/PartTimeConnect_backend/collab/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import recommendations_api
-
-urlpatterns = [
-    path("api/recommendations/", recommendations_api, name="recommendations_api"),
-]
--- part-time-connect-backend_django/PartTimeConnect_backend/PartTimeConnect_backend/popularity_recommendation/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('jobs/', views.job_seeker_recommendations),
-    path('candidates/', views.recruiter_recommendations),
-    path('candidates/<int:job_id>/', views.recruiter_recommendations),
-
-]
